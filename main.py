8c17220799382b6efb276faa2503a286dac59f7f
Camila Gallo <camilagallo1700@gmail.com>
2025-10-16 12:41:54 -0300

    seguro instalé mal algo o tengo un path mal o es un problema de versiones

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import tree_sitter
 import tree_sitter_circom
 
-from tree_sitter import Language, Tree, Node, Parser
+
+CIRCOM = tree_sitter.Language(tree_sitter_circom.language())
+
+parser = tree_sitter.Parser()
+parser.language = CIRCOM
+
+
+source_code = b"""
+template Adder() {
+    signal input a, b;
+    signal output c;
+    c <== a + b;
+}
+"""
+
+tree = parser.parse(source_code)
+root_node = tree.root_node
+print(root_node.str())
