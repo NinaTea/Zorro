553963783ac8b97a01c90558b8040c10ddd4da1c
Camila Gallo <camilagallo1700@gmail.com>
2025-10-17 11:22:42 -0300

    algo lee

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import tree_sitter
-import tree_sitter_circom
+import tree_sitter_circom as tscircom
+from tree_sitter import Language, Parser
 
+# TODO FIX because this is deprecated
+CIRCOM = Language(
+    "tree-sitter-circom/build/lib.linux-x86_64-cpython-310/tree_sitter_circom/_binding.abi3.so", "circom")
 
-CIRCOM = tree_sitter.Language(tree_sitter_circom.language())
-
-parser = tree_sitter.Parser()
-parser.language = CIRCOM
-
+parser = Parser()
+parser.set_language(CIRCOM)
 
 source_code = b"""
 template Adder() {
@@ -16,6 +16,22 @@ template Adder() {
 }
 """
 
+
 tree = parser.parse(source_code)
 root_node = tree.root_node
-print(root_node.str())
+
+# Print the syntax tree
+print(root_node.sexp())
+
+# You can also traverse the tree
+
+
+# def print_tree(node, indent=0):
+#     print("  " * indent +
+#           f"{node.type}: {node.text if len(node.children) == 0 else ''}")
+#     for child in node.children:
+#         print_tree(child, indent + 1)
+
+
+# print("\n--- Tree Structure ---")
+# print_tree(root_node)
